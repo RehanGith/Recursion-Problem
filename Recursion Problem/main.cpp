1c77e44703c5df4f7695ea50dd5742efda282404
RehanGith <chrehan1007@gmail.com>
2024-04-05 19:23:44 +0500

    create factorial, power, sum function

--- Recursion Problem/main.cpp
+++ Recursion Problem/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
 using namespace std;
+int sum(int num) {
+	if (num == 1)
+		return num;
+	return sum(num - 1) + num;
+}
+int factorial(int num) {
+	if (num <= 1)
+		return 1;
+	return factorial(num - 1) * num;
+}
+int power(int num, int exp) {
+	if (exp <= 1)
+		return num;
+	return power(num, exp-1) * num;
+}
 
 int main() {
-
+	cout << sum(4) << endl;
+	return 0;
 }
